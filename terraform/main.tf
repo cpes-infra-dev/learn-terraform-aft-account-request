b01459688466fd8dbbef552f4a2ef4dcb68858d3
marklehmann26 <mark@mksvr01.com>
2025-02-10 22:50:55 +0000

    paul account creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,33 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+#Paul Hammond Account
+
+module "sandbox_account_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cpes-infra+sandbox-paul-hammond@centricsoftware.onmicrosoft.com"
+    AccountName               = "sandbox-paul-hammond"
+    ManagedOrganizationalUnit = "DEV Account Factory"
+    SSOUserEmail              = "cpes-infra+sandbox-paul-hammond@centricsoftware.onmicrosoft.com"
+    SSOUserFirstName          = "Paul"
+    SSOUserLastName           = "Hammond"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "DevOps"
+    change_reason       = "New AWS Control Tower Account"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
